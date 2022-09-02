4a7616b1e347b8dc68e05e863d949a017114bf09
gkgg123 <gkgg123@naver.com>
2022-09-02 19:39:30 +0900

    1076 solved
    1076 solved

---
+++ 1076_저항_version1.py
@@ -0,0 +1,11 @@
+color = ['black',
+"brown",
+"red",
+"orange",
+"yellow",
+"green",
+"blue",
+"violet",
+"grey",
+"white"]
+print((10*color.index(input())+ color.index(input()))* 10**color.index(input()))
